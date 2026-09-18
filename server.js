493e7d3f1636467360610e316e479cfad70255ee
siddharth Sid <siddhu81431@gmail.com>
2026-09-18 23:25:23 +0530

    Create app server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/health', (_req,res)=>res.json({status:'ok'}));
+app.listen(PORT, ()=>console.log(`Bike Petrol App running on ${PORT}`));
